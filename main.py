3abe2df5262a0854e88da95a0697f5ef2a34b3f5
aliprf <aliprf@gmail.com>
2025-06-11 01:24:36 -0600

    fix: updated main

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-
 import threading
-from server.server import app  
-from client.client import build_ui
 
-def run_flask():
+from client.client import build_ui
+from server.server import app
+
+
+def run_flask()-> None:
     app.run(port=5000, debug=False, use_reloader=False)
 
-def run_gradio():
+
+def run_gradio() -> None:
     ui = build_ui()
     ui.launch(server_name="0.0.0.0", server_port=7860)
+
 
 if __name__ == "__main__":
     t1 = threading.Thread(target=run_flask)
     t1.start()
-    run_gradio() 
+    run_gradio()
